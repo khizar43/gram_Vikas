c1fd292c53775b63fecc71e7956bbf8330398067
surajit mondal <surajit.mondal@pratian.com>
2023-11-20 18:14:04 +0530

    villege profile screen done

--- src/app/gram-vikas/villages/villages-routing.module.ts
+++ src/app/gram-vikas/villages/villages-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddVillageComponent } from './components/add-village/add-village.component';
 import { HomeComponent } from '../dashboard/components/home/home.component';
+import { VillageDashboardComponent } from './components/village-dashboard/village-dashboard.component';
 
 const routes: Routes = [
-  
+  {
+    path:'villages',
+    loadChildren:()=>import('../villages/villages.module').then(m=>m.VillagesModule), 
+  },
     {
       path: '',
       component: HomeComponent
@@ -12,7 +16,11 @@ const routes: Routes = [
     {
     path: 'add-village',
     component:AddVillageComponent,
-  }
+  },
+  {
+    path: 'profile',
+    component:VillageDashboardComponent
+  },
 ];
 
 @NgModule({

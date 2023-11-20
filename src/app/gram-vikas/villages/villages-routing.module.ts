5ecf18723d39737fb84913abcc62373c2c814827
pappu shelake <maulishelake75@gmail.com>
2023-11-20 20:02:27 +0530

    routing has be done for authentication module

--- src/app/gram-vikas/villages/villages-routing.module.ts
+++ src/app/gram-vikas/villages/villages-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VillageStatusComponent } from './components/village-status/village-status.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

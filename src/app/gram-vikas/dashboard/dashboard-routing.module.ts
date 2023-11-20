34e380a3ad6509f5601716936411e7ee1c1415d8
Puja Achalere <Puja.Achalere@pratian.com>
2023-11-20 10:36:36 +0530

    village details-villagers

--- src/app/gram-vikas/dashboard/dashboard-routing.module.ts
+++ src/app/gram-vikas/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,6 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { VillageComponent } from './components/village/village.component';
-import { HomeComponent } from './components/home/home.component';
 import { AddVillageComponent } from '../villages/components/add-village/add-village.component';
 
 const routes: Routes = [

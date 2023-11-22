8d03e125aafe3901daaefe77c16cb2ca0bfc0f74
ChakrapaniReddi <Chakrapani.Reeddi@skillassure.com>
2023-11-22 10:48:34 +0530

    show and hide formDetails function

--- src/app/gram-vikas/villagers/add-villagers/components/farming/farming.component.ts
+++ src/app/gram-vikas/villagers/add-villagers/components/farming/farming.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class FarmingComponent {
 
+
+showFormData:boolean = false;
+
+
+ 
+formDataVisibility() {
+  this.showFormData = !this.showFormData;
+}
+
+
 }

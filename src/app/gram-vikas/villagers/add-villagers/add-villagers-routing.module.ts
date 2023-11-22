30dfcdd66a7a1e9b73fda9eb85407d0aa07493fd
Lakshmana-725 <lakshmanp973@gmail.com>
2023-11-22 19:08:49 +0530

    family-profile

--- src/app/gram-vikas/villagers/add-villagers/add-villagers-routing.module.ts
+++ src/app/gram-vikas/villagers/add-villagers/add-villagers-routing.module.ts
@@ -7,15 +7,17 @@ import { FarmingComponent } from './components/farming/farming.component';
 import { WastePlasticManagementComponent } from './components/waste-plastic-management/waste-plastic-management.component';
 import { FamilyMemberDetailsComponent } from './components/family-member-details/family-member-details.component';
 import { ProductivityComponent } from './components/productivity/productivity.component';
+import { FamilyProfileComponent } from './components/family-profile/family-profile.component';
 
 const routes: Routes = [
   { path: '', component: VillageDetailsComponent },
   { path: 'family-Details', component: FamilyDetailsComponent },
   { path: 'farming', component: FarmingComponent },
-  { path: 'productivity', component: ProductivityComponent},
+  { path: 'productivity', component: ProductivityComponent },
   { path: 'resources', component: ResourcesComponent },
   { path: 'waste-plastic-management', component: WastePlasticManagementComponent },
-  { path: 'family-member-details', component: FamilyMemberDetailsComponent},
+  { path: 'family-member-details', component: FamilyMemberDetailsComponent },
+  { path: 'family-profile', component: FamilyProfileComponent },
 
 
 ];

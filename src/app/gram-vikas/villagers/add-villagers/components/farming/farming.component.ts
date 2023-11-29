c1f792cd73040ad421c992edeec4fbb3fb478dae
ChakrapaniReddi <Chakrapani.Reeddi@skillassure.com>
2023-11-29 12:07:44 +0530

    changes updated farming and productivity

--- src/app/gram-vikas/villagers/add-villagers/components/farming/farming.component.ts
+++ src/app/gram-vikas/villagers/add-villagers/components/farming/farming.component.ts
@@ -7,13 +7,30 @@ import { Component } from '@angular/core';
 })
 export class FarmingComponent {
 
+// show or hide the farming  FarmData
+showformDataYes: boolean = false;
+showformDataNo: boolean = false;
 
-showFormData:boolean = false;
+//  showFarming Form Data
+ formDataVisibilityYes() {
+    this.showformDataYes = !this.showformDataYes;
+    this.showformDataNo = false;
+    this.activeButton;    //currently activeButton
+  }
+//  hideFarming Form Data
+  formDataVisibilityNo() {
+    this.showformDataNo = !this.showformDataNo;
+    this.showformDataYes = false;
+    this.activeButton;    //currently activeButton
+  }
 
+// Active Buttons
+activeButton: number = 0;
 
- 
-formDataVisibility() {
-  this.showFormData = !this.showFormData;
+// Set the activeButton property to the provided buttonNumber value
+
+setActiveButton(buttonNumber: number) {
+  this.activeButton = buttonNumber;
 }
 
 

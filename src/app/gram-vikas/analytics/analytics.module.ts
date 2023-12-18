7de311616ecf06077a5487d6c82447189a5be98f
Puja Achalere <Puja.Achalere@pratian.com>
2023-12-18 13:01:13 +0530

    components added in analytics

--- src/app/gram-vikas/analytics/analytics.module.ts
+++ src/app/gram-vikas/analytics/analytics.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticsRoutingModule } from './analytics-routing.module';
+import { AnalyticsDashboardComponent } from './components/analytics-dashboard/analytics-dashboard.component';
 
 
 @NgModule({
   declarations: [
     
+  
+    AnalyticsDashboardComponent
   ],
   imports: [
     CommonModule,
